a465d97a7a2f8b36fefa531f98fe03b42cbc7b7b
suri312006 <suri312006@gmail.com>
2024-01-29 09:25:13 -0800

    now fixed the thing

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,5 @@
 import { clerkClient } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/server";
+import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -55,7 +55,7 @@ export const postRouter = createTRPCRouter({
     // whoa what the fuck is that parentesis before the object notation?? oh its an implicit return??
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId)
-      if (!author || !author.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Author for message not found" }
+      if (!author?.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: "Author for message not found" }
       );
       return {
         post,
